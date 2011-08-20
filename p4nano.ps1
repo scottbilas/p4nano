cdb308bf4a6293b091e05659427ebdee850667f3
Scott Bilas <scottbilas@gmail.com>
2011-08-20 09:06:24 -0700

    split out

--- p4nano.ps1
+++ p4nano.ps1
@@ -1,4 +1,10 @@
-$p4nanodebug = $true
+##### REMOVE ?? and ?: USAGE
+
+
+# enable this when working on p4nano.cs
+#$p4nanodebug = $true
+
+<### Compile p4nano.cs ###>
 
 $p4nanocp = new-object codedom.compiler.compilerparameters
 $p4nanocp.ReferencedAssemblies.Add('system.dll') > $null
@@ -22,9 +28,13 @@ if ($p4nanodebug) {
 $p4nanoscript = [io.file]::readalltext((split-path $myinvocation.mycommand.definition) + '\p4nano.cs')
 add-type $p4nanoscript -lang csharpversion3 -compilerparam $p4nanocp
 
+<### Configure p4nano ###>
+
 # this isn't working right now..have to figure out how to get posh to either pass a ctrl-c
 # or dispose outstanding enumerables when it kills the script.
 [p4nano.record]::CancelOnCtrlC = $true
+
+<### Primary entry points ###>
 
 filter p4-run {
     $input = $_
@@ -151,9 +161,13 @@ filter p4n-run {
 set-alias p4 p4-run
 set-alias p4n p4n-run
 
+<### Format helpers ###>
+
 function Output-String {
     process { $_.tostring() }
 }
+
+set-alias ostr output-string
 
 function Output-FormString {
     process { $_.toformstring() }
@@ -165,185 +179,4 @@ function ConvertDateTime-P4ToSystem($p4date) {
 
 function ConvertDateTime-SystemToP4($datetime) {
     return [p4nano.utility]::systemtop4($datetime)
-}
-
-set-alias ostr output-string
-
-function p4n-snapshot-create {
-	$info = p4n info
-
-	$latest = 0
-	$prefix = $info.clientname + '_snapshot_'
-
-	$labels = p4n labels -e ($prefix + '*')
-
-	"Found $(?? $labels.count 0) existing labels created by user"
-
-	$labels | ?{$_.label -match '\d+$'} | %{
-	    $serial = [int]$matches[0]
-	    if ($serial -ge $latest) {
-	        $latest = $serial + 1
-	    }
-	}
-
-	$l = new-object p4nano.record
-	$l.Label = $prefix + $latest
-	$l.Description = "Snapshot label created for $($info.username) on $(get-date)"
-	$l.Options = 'unlocked'
-	$l.Owner = $info.username
-	$l.arrayfields.set('View', '//depot/...')
-
-	"Created label $($l.label)"
-
-	$l | p4n label -i > $null
-
-	"Copying client state to label..."
-
-	p4 labelsync -q -l $l.Label
-
-	"Done!"
-}
-
-function p4n-snapshot-list {
-	$info = p4n info
-
-	$prefix = $info.clientname + '_snapshot_'
-	p4n labels -e ($prefix + '*') | %{$_.label} | sort -prop @{ Expression = {
-		[int]::parse([regex]::match($_, '(\d+)$').groups[1])
-	}}
-}
-
-filter p4n-snapshot-delete(
-	[parameter(Mandatory=$true,ValueFromPipeline=$true)]
-	[int]$serial) {
-    if ($_ -ne $null) { $serial = $_ }
-
-	$info = p4n info
-
-	$prefix = $info.clientname + '_snapshot_'
-	(p4n label -d ($prefix + $serial)).data
-}
-
-filter p4n-snapshot-delete-old([int]$KeepCount = 5) {
-	$snapshots = p4n-snapshot-list | %{[int]::parse([regex]::match($_, '(\d+)$').groups[1])}
-	for ($i = 0; $i -lt $snapshots.count - $keepcount; ++$i) {
-		p4n-snapshot-delete $snapshots[$i]
-	}
-}
-
-function p4n-cdiff([int]$changenum) {
-
-	# TODO future: detect when the have version == the 'new' version, the 'new' version is not checked out locally, and if so use the workstation path instead. that way can edit the file directly in bc.
-	# TODO future: have an option to just diff everything vs. local copy
-	# TODO future: have an option to ignore insignificant changes (will require examining diff2 output) like whitespace-only changes, 'using namespace'-only changes..
-	# TODO future: have an option to batch them into sets of say 10 files per bcompare.exe instance. really not too useful when have a billion files and can't see which is which.
-    # TODO future: accept path args to filter the changelist
-
-	$status = 'pending'
-	if ($changenum) { $status = (p4n change -o $changenum).status }
-
-	if ($status -eq 'pending') {
-
-	    $shelved = $false
-
-	    if ($changenum) {
-	        $files = p4n -nowarn fstat -Ro -e $changenum //...
-	        # try shelved
-	        if ($files.iserror) {
-	            $files = p4n fstat -Rs -e $changenum //...
-	            $shelved = $true
-	        }
-	    }
-	    else {
-	        $files = p4n fstat -Ro //...
-	    }
-
-		#####WIP...
-        if (!$files.iserror) {
-    	    "IMPLEMENT ME!!"
-        }
-        else {
-            $files
-        }
-	}
-	elseif ($status -eq 'submitted') {
-
-	    $p4diff = $null
-	    $isbc = $false
-
-	    if ((p4 set p4diff) -match '=(.*?)(\s+\([^)]*\))?$') {
-	        $p4diff = $matches[1]
-	        if ((split-path -leaf $p4diff) -eq 'bcomp.exe') {
-				$isbc = $true
-				# switch to bcompare.exe to cut down on processes - we aren't waiting for bcomp.exe to quit
-				$p4diff = (split-path $p4diff) + '\bcompare.exe'
-	        }
-	    }
-
-	    (p4n describe -s $changenum).items | %{
-
-	        $olddepot = "$($_.depotfile)#$($_.rev-1)"
-	        $newdepot = "$($_.depotfile)#$($_.rev)"
-
-            if ($_.action -eq 'add') {
-                $olddepot = $null
-            }
-            elseif ($_.action -eq 'delete') {
-                $newdepot = $null
-            }
-            elseif ($_.action -ne 'edit') {
-                write-error "action type $($_.action) currently unsupported (for $($_.depotfile))"
-                continue
-            }
-
-	        if ($p4diff) {
-
-	            $tempname = [io.path]::gettemppath() + 'p4n\' + $_.depotfile.substring(2).replace('/', '\')
-	            $path = split-path $tempname
-	            $filename = split-path -leaf $tempname
-	            $ext = [io.path]::getextension($filename)
-	            $nameonly = [io.path]::getfilenamewithoutextension($filename)
-
-	            $oldfile = "$path\$nameonly#$($_.rev-1)$ext"
-
-                if ($olddepot) {
-    	            $oldtitle = "$filename - $olddepot"
-                    p4 print -q -o $oldfile $olddepot
-                }
-                else {
-    	            $oldtitle = "$filename - (new file)"
-                    if (test-path $oldfile) { del -force $oldfile }
-                }
-
-	            $newfile = "$path\$nameonly#$($_.rev)$ext"
-
-                if ($newdepot) {
-    	            $newtitle = "$filename - $newdepot"
-	                p4 print -q -o $newfile $newdepot
-                }
-                else {
-                    $newtitle = "$filename - (deleted file)"
-                    if (test-path $newfile) { del -force $newfile }
-                }
-
-	            # special support for bc
-                $diffargs = @()
-	            if ($isbc) {
-                    $diffargs += '/ro', "/title1=$oldtitle", "/title2=$newtitle"
-	            }
-                $diffargs += $oldfile
-                if ($newdepot) {
-                    $diffargs += $newfile
-                }
-                &$p4diff $diffargs
-
-				$newdepot
-	            sleep .1 # sleep a bit to let bcomp keep up, so the files stay in the order we want
-	        }
-	        else {
-	            # fall back to meh behavior
-	            p4 diff2 $olddepot $newdepot
-	        }
-	    }
-	}
 }
